d416dde9a7f0c8c261a9c6cb80e94c129d299ed0
leihuazhe <295482300@qq.com>
2018-08-20 20:58:23 +0800

    java版配置 spring mvc

--- src/main/java/com/maple/mvc/controller/async/AsyncHelloController.java
+++ src/main/java/com/maple/mvc/controller/async/AsyncHelloController.java
@@ -1,7 +1,11 @@
 package com.maple.mvc.controller.async;
 
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import java.util.concurrent.Callable;
 
 /**
  * desc: AsyncHelloController
@@ -9,11 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
  * @author hz.lei
  * @since 2018年08月20日 上午10:02
  */
-@RestController
+//@RestController
 @Async
 public class AsyncHelloController {
 
+//    @RequestMapping("/async")
+    public Callable<String> async() throws InterruptedException {
+        Callable<String> callable = () -> {
+            Thread.sleep(5000);
 
+//            logger.info("实际工作执行完成！");
+
+            return "succeed!";
+        };
+        Thread.sleep(20000);
+        return callable;
+    }
 
 
 }
---
+++ src/main/java/com/maple/mvc/service/LongTermTaskCallback.java
@@ -0,0 +1,11 @@
+package com.maple.mvc.service;
+
+/**
+ * desc: callback
+ *
+ * @author hz.lei
+ * @since 2018年08月20日 下午5:05
+ */
+public interface LongTermTaskCallback {
+    void callback(Object result);
+}
