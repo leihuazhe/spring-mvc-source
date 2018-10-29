94e5072f5d59485b0e4c45776df85d2bd9ebe0a3
leihuazhe <295482300@qq.com>
2018-10-29 13:13:33 +0800

    添加异常处理器

---
+++ src/main/java/com/maple/mvc/controller/ExController.java
@@ -0,0 +1,33 @@
+package com.maple.mvc.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author <a href=mailto:leihuazhe@gmail.com>maple</a>
+ * @since 2018-10-29 11:17 AM
+ */
+@Controller
+@RequestMapping(value = "exception")
+public class ExController {
+
+
+    @ExceptionHandler({NullPointerException.class})
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public String handleNullPointerException(Exception e) {
+        e.printStackTrace();
+        return "error";
+    }
+
+    @RequestMapping(value = "e3/{id}", method = {RequestMethod.GET})
+    @ResponseBody
+    public String testExceptionHandle3(@PathVariable(value = "id") Integer id) {
+        List<String> list = 4 % id == 0 ? null : Arrays.asList("a", "b", "c", "d");
+        return list.get(id);
+    }
+}
